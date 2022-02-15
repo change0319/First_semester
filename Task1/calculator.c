210ca0f4de5dad8a5e9e343276d662953ee7eb68
change0319 <89935683+change0319@users.noreply.github.com>
2022-02-15 23:58:44 +0500

    Add files via upload

---
+++ Task1/calculator.c
@@ -0,0 +1,55 @@
+#include "magic.h"
+#include "secret-level.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int firstOperand = 0, secondOperand = 0, result = 0;
+    char operation = 0;
+    if (argc == 4) {
+        // некоторая "магия", которую не нужно трогать
+        if (initMagic(argc, argv, &firstOperand, &secondOperand, &operation) != EXIT_SUCCESS) {
+            printf_s("ERROR\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        //ручной ввод
+        if (scanf_s("%d%c%c%d", &firstOperand, &operation, 1, &operation, 1, &secondOperand) != 4) {
+            printf_s("ERROR: WRONG INPUT\nEXIT WITH ERROR CODE 1\n");
+            return 1;
+        }
+        // не тратьте время на следующую строку пока не выполнили основное задание
+        if (firstOperand == I_AM_READY_NOW) tenThousandsYears();
+    }
+    switch (operation) {
+    case '+':
+        result = firstOperand + secondOperand;
+        break;
+    case '-':
+        result = firstOperand - secondOperand;
+        break;
+    case '*':
+        result = firstOperand * secondOperand;
+        break;
+    case '/':
+        result = firstOperand / secondOperand;
+        break;
+    case '%':
+        result = firstOperand % secondOperand;
+        break;
+    case '^':
+        result = 1;
+        for (int i = 0; i < secondOperand; ++i) {
+            result *= firstOperand;
+        }
+        break;
+    default:
+        printf_s("ERROR: UNKNOWN OPERATION\nEXIT WITH ERROR CODE 1\n");
+    }
+    printf_s("%d\n", result);
+    if (argc != 4) {
+        system("pause");
+    }
+    return EXIT_SUCCESS;
+}
---
+++ Task1/magic.h
@@ -0,0 +1,2 @@
+#pragma once
+int initMagic(int argc, char** argv, int* firstOp, int* secondOp, char* operation);
